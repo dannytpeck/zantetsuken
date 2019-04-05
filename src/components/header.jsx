ff23347adb69e7951698b583fea2828885b139e5
Danny Peck <dannytpeck@gmail.com>
2019-04-05 11:44:24 +0700

    Add katana image and icons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <header id="header">
-        {/* <img src="images/logo.svg" /> */}
+        <img src="images/logo.svg" />
         <h1 className="title">Zantetsuken</h1>
         <h3>Cut Through Anything</h3>
       </header>
