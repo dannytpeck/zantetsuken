ff7b0607a8883749a014f0e267726dc9b0c870c9
Danny Peck <dannytpeck@gmail.com>
2020-06-08 14:32:59 -0700

    Completely revamp to do something different

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,47 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import Airtable from 'airtable';
+const base = new Airtable({ apiKey: 'keyCxnlep0bgotSrX' }).base('appHXXoVD1tn9QATh');
 
 import Header from './header';
 import Footer from './footer';
-import Modal from './modal';
-import Tile from './tile';
 
 import updateAllHotTopics from '../helpers/update_all_hot_topics';
 
+function reducer(state, action) {
+  return [...state, ...action];
+}
+
 /* globals $ */
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [clients, dispatchClients] = React.useReducer(
+    reducer,
+    [] // initial clients
+  );
+  const [activities, dispatchActivities] = React.useReducer(
+    reducer,
+    [] // initial activities
+  );
 
-    this.state = {
-      clients: [],
-      selectedClient: null,
-      tiles: []
-    };
-  }
+  // When app first mounts, fetch clients
+  useEffect(() => {
 
-  componentDidMount() {
-    $.getJSON('https://api.airtable.com/v0/appHXXoVD1tn9QATh/Clients?api_key=keyCxnlep0bgotSrX&view=sorted').done(data => {
-      let records = data.records;
+    base('Clients').select({
+      view: 'sorted'
+    }).eachPage((records, fetchNextPage) => {
+      dispatchClients(records);
 
-      if (data.offset) {
-        $.getJSON(`https://api.airtable.com/v0/appHXXoVD1tn9QATh/Clients?api_key=keyCxnlep0bgotSrX&view=sorted&offset=${data.offset}`).done(data => {
-          this.setState({
-            clients: [...records, ...data.records]
-          });
-        });
-      } else {
-        this.setState({
-          clients: records
-        });
+      fetchNextPage();
+    }, (err) => {
+      if (err) {
+        console.error(err);
+        return;
       }
-
     });
-  }
 
-  getActivityLifecycle() {
-    this.state.clients.forEach(client => {
+  }, []); // Pass empty array to only run once on mount
+
+  function getActivities() {
+
+    // Set counter based on # of clients
+    $('#counter').html(`<p><span id="finishedUploads">0</span> / ${clients.length}</p>`);
+
+    clients.map(client => {
+
       if (client.fields['LimeadeAccessToken']) {
-        console.log('Getting all activities for ' + client.fields['Account Name']);
+
         $.ajax({
           url: 'https://api.limeade.com/api/admin/activity',
           type: 'GET',
@@ -51,10 +59,17 @@ class App extends Component {
           },
           contentType: 'application/json; charset=utf-8'
         }).done(result => {
-          const tiles = result.Data;
 
-          // Place mass updater function(s) here
-          updateAllHotTopics(client, tiles);
+          // Advance the counter
+          let count = Number($('#finishedUploads').html());
+          $('#finishedUploads').html(count + 1);
+
+          result.Data.map(activity => {
+            if (activity.Name.includes('Not Alone: Mental Health')) {
+              activity.client = client;
+              dispatchActivities([...activities, activity]);
+            }
+          });
 
         }).fail((xhr, textStatus, error) => {
           console.error(`${client.fields['Account Name']} - GET ActivityLifecycle has failed`);
@@ -63,131 +78,112 @@ class App extends Component {
       } else {
         console.error(`${client.fields['Account Name']} has no LimeadeAccessToken`);
       }
+
     });
+
   }
 
-  getActivities() {
-    this.state.clients.forEach(client => {
-      if (client.fields['LimeadeAccessToken']) {
-        console.log('Getting all activities for ' + client.fields['Account Name']);
-        $.ajax({
-          url: 'https://api.limeade.com/api/activities/?types=5&status=1&attributes=1&contents=32319',
-          type: 'GET',
-          dataType: 'json',
-          headers: {
-            Authorization: 'Bearer ' + client.fields['LimeadeAccessToken']
-          },
-          contentType: 'application/json; charset=utf-8'
-        }).done(result => {
-          const tiles = result.Data;
-
-          // Place mass updater function(s) here
-
-          function isItAnHpTile(tile) {
-            return tile.Title.includes('Health &') ||
-                   tile.Title.includes('Growth &') ||
-                   tile.Title.includes('Money &') ||
-                   tile.Title.includes('Contribution &');
-          }
-
-          const filteredTiles = tiles.filter(isItAnHpTile);
-
-          console.log(client.fields['Account Name'], filteredTiles);
-
-        }).fail((xhr, textStatus, error) => {
-          console.error(`${client.fields['Account Name']} - GET Activities has failed`);
-        });
-
-      } else {
-        console.error(`${client.fields['Account Name']} has no LimeadeAccessToken`);
-      }
-    });
-  }
-
-  getActivitiesForOneClient(client) {
-    if (client) {
-      if (client.fields['LimeadeAccessToken']) {
-        console.log('Getting visible Home page activities for ' + client.fields['Account Name']);
-        $.ajax({
-          url: 'https://api.limeade.com/api/activities/?types=5&status=1&attributes=1&contents=32319',
-          type: 'GET',
-          dataType: 'json',
-          headers: {
-            Authorization: 'Bearer ' + client.fields['LimeadeAccessToken']
-          },
-          contentType: 'application/json; charset=utf-8'
-        }).done(result => {
-          const tiles = result.Data;
-
-          // Place mass updater function(s) here
-          console.log(tiles);
-          this.setState({ tiles: tiles });
-
-        }).fail((xhr, textStatus, error) => {
-          console.error(`${client.fields['Account Name']} - GET Activities has failed`);
-        });
-
-      } else {
-        console.error(`${client.fields['Account Name']} has no LimeadeAccessToken`);
-      }
+  function performUpdate(activity) {
+    if (activity.AboutChallenge.includes('anxiety_antidote')) {
+      console.log('anxiety_antidote found, update not needed');
     } else {
-      console.log('No client has been selected');
+
+      const employerName = activity.client.fields['Limeade e='];
+      const psk = activity.client.fields['Limeade PSK'];
+
+      const csv = createCSV(activity);
+      const url = 'https://calendarbuilder.dev.adurolife.com/limeade-upload/';
+
+      const params = {
+        e: employerName,
+        psk: psk,
+        data: csv.join('\n'),
+        type: 'IncentiveEvents'
+      };
+
+      $.post(url, params).done((response) => {
+        $('#' + employerName.replace(/\s*/g, '')).addClass('bg-success text-white');
+      }).fail((request, status, error) => {
+        $('#' + employerName.replace(/\s*/g, '')).addClass('bg-danger text-white');
+        console.error(request.status);
+        console.error(request.responseText);
+        console.log('Update CIE failed for client ' + employerName);
+      });
+
     }
   }
 
-  setSelectedClient(e) {
-    this.state.clients.forEach((client) => {
-      if (client.fields['Limeade e='] === e.target.value) {
-        this.setState({ selectedClient: client });
+  function renderActivities() {
+
+    // Sorts the list of activities by client's account name
+    const sortedActivities = [...activities];
+
+    sortedActivities.sort((a, b) => {
+      const nameA = a.client.fields['Account Name'].toLowerCase();
+      const nameB = b.client.fields['Account Name'].toLowerCase();
+      if (nameA < nameB) {
+        return -1;
       }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
     });
+
+    return sortedActivities.map((activity) => {
+      const employerName = activity.client.fields['Limeade e='];
+      const domain = activity.client.fields['Domain'];
+      const eventId = activity.ChallengeId * -1;
+
+      const hasApp = activity.AboutChallenge.match(/Resources in the Aduro App/g);
+      const hasCoaching = activity.AboutChallenge.match(/Connect to coach support/g);
+
+      return (
+        <tr id={employerName.replace(/\s*/g, '')} key={employerName}>
+          <td>{activity.client.fields['Account Name']}</td>
+          <td><a href={`${domain}/Home/?cid=${eventId}`} target="_blank">{activity.Name}</a></td>
+          <td>
+            {
+              hasApp ? 'Coaching + App' :
+                hasCoaching ? 'No App' : 'No Coaching'
+            }
+          </td>
+          <td>
+            <button type="button" className="btn btn-primary" onClick={() => performUpdate(activity)}>Update</button>
+          </td>
+        </tr>
+      );
+    });
+
   }
 
-  renderEmployerNames() {
-    return this.state.clients.map((client) => {
-      return <option key={client.id}>{client.fields['Limeade e=']}</option>;
-    });
-  }
+  return (
+    <div id="app">
+      <Header />
 
-  renderTiles() {
-    return this.state.tiles.map((tile) => {
-      return <Tile key={tile.Id} tile={tile} />;
-    });
-  }
-
-  render() {
-    return (
-      <div id="app">
-        <Header />
-
-        <section id="massUpdater">
-          <button type="button" className="btn btn-primary" onClick={() => this.getActivityLifecycle()}>Mass Update - ActivityLifecycle API</button>
-          <p>(pulls everything - past, present, and future - Even legacy CIEs)</p>
-
-          <button type="button" className="btn btn-primary" onClick={() => this.getActivities()}>Mass Update - Activities API</button>
-          <p>(what's visible in the platform)</p>
-        </section>
-
-        <div className="form-group">
-          <label htmlFor="employerName">EmployerName</label>
-          <select id="employerName" className="form-control custom-select" onChange={(e) => this.setSelectedClient(e)}>
-            <option defaultValue>Select Employer</option>
-            {this.renderEmployerNames()}
-          </select>
-        </div>
-
-        <button type="button" className="btn btn-primary" onClick={() => this.getActivitiesForOneClient(this.state.selectedClient)}>What on My Home Page?</button>
-        <p>(show's the home page as seen by the Admin)</p>
-
-        <div id="tileContainer">
-          {this.renderTiles()}
-        </div>
-
-        <Footer />
-        <Modal />
+      <div className="form-group">
+        <button type="button" className="btn btn-primary" onClick={getActivities}>Download Activities</button>
+        <div id="counter"></div>
       </div>
-    );
-  }
+
+      <table className="table table-hover table-striped" id="activities">
+        <thead>
+          <tr>
+            <th scope="col">Account Name</th>
+            <th scope="col">Name</th>
+            <th scope="col">Type</th>
+            <th scope="col">Update</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderActivities()}
+        </tbody>
+      </table>
+
+      <Footer />
+
+    </div>
+  );
 }
 
 export default App;
